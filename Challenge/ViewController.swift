84a9834d0b94929639691f055118205402883b84
pranu <reddi.pranavi@gmail.com>
2021-07-01 13:36:31 -0500

    Challenge Initial; links need to be added

--- Challenge/ViewController.swift
+++ Challenge/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
+
 
 class ViewController: UIViewController {
 
@@ -14,6 +17,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear( animated)
+        
+        let player = AVPlayer(url: URL (fileURLWithPath: Bundle.main.path(forResource: "Double Wear", ofType: "mov")!))
+        
+        let layer = AVPlayerLayer(player: player)
+        
+        layer.frame = view.bounds
+        
+        
+        view.layer.addSublayer(layer)
+        
+        player.play()
+        
+        
+    }
 
 }
+
 
